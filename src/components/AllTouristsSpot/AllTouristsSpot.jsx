ae8435442a6a96d641aca7c5ff74350b17c77ea9
Tarek <114747789+Tarek124@users.noreply.github.com>
2024-04-28 15:02:21 +0600

    sort method added

--- src/components/AllTouristsSpot/AllTouristsSpot.jsx
+++ src/components/AllTouristsSpot/AllTouristsSpot.jsx
@@ -4,36 +4,26 @@ import { useState } from "react";
 
 export default function AllTouristsSpot() {
   const data = useLoaderData();
-  const [filter, setFilter] = useState("");
   const [userData, setUserData] = useState(data);
 
-  const handleFilter = (e) => {
-    setFilter(e.target.value);
-    if (filter == "") {
-      setUserData(data);
-    } else if (filter == "Visitor Per Year") {
-      const newData = data?.totaVisitorsPerYear?.sort((a, b) => b - a);
-      setUserData(newData);
-      console.log(newData);
-    } else {
-      const newData = data?.average_cost?.sort((a, b) => b - a);
-      setUserData(newData);
-      console.log(newData);
-    }
+  const handleSort = (name) => {
+    const sorted = [...userData].sort((a, b) => b[name] - a[name]);
+    setUserData(sorted);
   };
 
   return (
     <div className="md:mt-12">
-      <select
-        value={filter}
-        onChange={handleFilter}
-        className="select select-bordered w-full max-w-xs "
-      >
-        <option selected>Filter</option>
-        <option value={"Average Cost"}>Average Cost</option>
-        <option value={"Visitor Per Year"}>Visitor Per Year</option>
-      </select>
-
+      <details className="dropdown ">
+        <summary className="m-1 btn green text-white">Filter</summary>
+        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+          <li onClick={() => handleSort("average_cost")}>
+            <a>Average Cost</a>
+          </li>
+          <li onClick={() => handleSort("totaVisitorsPerYear")}>
+            <a>Visitor Per Year</a>
+          </li>
+        </ul>
+      </details>
       <div className="grid md:grid-cols-2 w-full gap-4">
         {userData?.map((item) => (
           <AllTouristsSpotCard key={item._id} item={item} />
